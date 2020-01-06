bc594eabf394e31e6f3b7c564cad46ce84dfeea2
mina <mina0774@hanmail.net>
2020-01-06 18:30:18 +0900

    add bottom bar

--- app/src/main/java/com/example/heronation/MainActivity.java
+++ app/src/main/java/com/example/heronation/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.heronation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
     }
 }

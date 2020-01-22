80d841167909024323adc2484c171aa506af8627
Lsyeon <baekgu1011@naver.com>
2020-01-22 18:33:24 +0900

    2020.01.22 스타일 AI 추천 추가

---
+++ app/src/main/java/com/example/heronation/RegisterAi.java
@@ -0,0 +1,24 @@
+package com.example.heronation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class RegisterAi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_ai);
+    }
+    //오류
+   // public void click_registerai_previous(View view){
+     //   Intent intent = new Intent(this, RegisterBodyActivity.class);
+       // startActivity(intent);
+    //}
+   // public void click_registerai_next(View view){
+     //   Intent intent = new Intent(this, IntroActivity.class);
+       // startActivity(intent);
+    //}
+}

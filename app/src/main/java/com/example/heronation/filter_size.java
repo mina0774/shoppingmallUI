787ff66fdc6a38d5aeca41f585c1dcfee9f6a0c6
mina <mina0774@hanmail.net>
2020-01-22 17:00:14 +0900

    filter

--- app/src/main/java/com/example/heronation/filter_size.java
+++ app/src/main/java/com/example/heronation/filter_size.java
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
 
 
 /**
@@ -66,7 +69,9 @@ public class filter_size extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter_size, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_filter_size, container, false);
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event

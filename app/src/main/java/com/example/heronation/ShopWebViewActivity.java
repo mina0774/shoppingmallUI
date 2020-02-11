9a981df6dc5fce8a38621833e14f9b9c324cc800
mina0774 <mina0774@hanmail.net>
2020-02-11 16:51:59 +0900

    check shop link url

--- app/src/main/java/com/example/heronation/ShopWebViewActivity.java
+++ app/src/main/java/com/example/heronation/ShopWebViewActivity.java
@@ -16,23 +16,31 @@ public class ShopWebViewActivity extends AppCompatActivity {
 
     @BindView(R.id.shop_web_view)
     WebView shop_web_view;
+    
+    // TODO: 2020-02-11 뒤로가기 버튼 눌렀을 때, 아이템 홈 화면 뜨는 것 처리하기 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_web_view);
         ButterKnife.bind(this);
+        String site_link;
+        if(!getIntent().getStringExtra("shop_link").contains("http")){
+            site_link="http://"+getIntent().getStringExtra("shop_link");
+        }else{
+            site_link=getIntent().getStringExtra("shop_link");
+        }
 
         shop_web_view.getSettings().setJavaScriptEnabled(true); //자바스크립트 허용
-        shop_web_view.loadUrl(getIntent().getStringExtra("shop_link")); //웹뷰 실행
-        shop_web_view.setWebChromeClient(new WebChromeClient()); //웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+        shop_web_view.loadUrl(site_link); //웹뷰 실행
+        shop_web_view.setWebChromeClient(new WebChromeClient()); //웹뷰에 크롬 사용 허용, 이 부분이 없으면 크롬에서 alert가 뜨지 않음
         shop_web_view.setWebViewClient(new WebViewClientClass()); //새 창 열기 없이 웹뷰 내에서 다시 열기, 페이지 이동 원활히 하기위해 사용
 
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && shop_web_view.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && shop_web_view.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로감
             shop_web_view.goBack();
             return true;
         }
